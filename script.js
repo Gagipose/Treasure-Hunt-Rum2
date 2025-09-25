3b19eb0fea4c2df2b9b43cfb15f64337ba50fe72
Benjame Abi <benjameabi@gmail.com>
2025-09-25 11:24:32 +0200

    skatt 2

--- script.js
+++ script.js
@@ -14,18 +14,50 @@ timer = setInterval(() => {
 }, 1000);
 
 // --- Här ska ni lägga in era egna skatter --- //
-// Exempel: skatt 1
+//skatt 1
 const treasure1 = document.getElementById("treasure1");
 treasure1.addEventListener("click", () => {
   treasure1.style.color = "red";
   foundCount++;
   foundCountEl.textContent = foundCount;
-  alert("Grattis! Du hittade en skatt!");
+  alert("Grattis! Du hittade skatt 1!");
 
   if (foundCount === 10) {
     clearInterval(timer);
     alert("Alla skatter hittade på " + time + " sekunder!");
   }
 });
+//skatt 2
+const treasure2 = document.getElementById("treasure2");
+treasure1.addEventListener("click", () => {
+  treasure1.style.color = "red";
+  // add highlight class for flash animation
+  treasure1.classList.add("highlight");
+
+  // remove it after 0.8s so it can be reapplied if needed
+  setTimeout(() => {
+    treasure1.classList.remove("highlight");
+  }, 800);
+  foundCount++;
+  foundCountEl.textContent = foundCount;
+  alert("Bra jobbat! Skatt 2 låg gömd mellan raderna");
+
+  if (foundCount === 10) {
+    clearInterval(timer);
+    alert("Alla skatter hittade på " + time + " sekunder!");
+  }
+});
+//skatt 3
+//skatt 4
+//skatt 5
+//skatt 6
+//skatt 7
+//skatt 8
+//skatt 9
+//skatt 10
+// Prevent double clicking
+
+
+// Winning condition
 
 // ... fortsätt för alla skatter ...
